e08aa8ba0ebfeda7f7a9d7252124d203a65c53db
GEOFREY KWEYU <38045476+KWEYUGEOFREY@users.noreply.github.com>
2021-09-19 02:28:24 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,3 @@
-#include "main.h"
 
 /**
  * _isupper - ch ecks for uppercase letters
@@ -15,3 +14,22 @@
  */
 
 
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
