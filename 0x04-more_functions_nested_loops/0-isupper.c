90c627cfdd67e5d605e50e625ed0fac52e160818
GEOFREY KWEYU <38045476+KWEYUGEOFREY@users.noreply.github.com>
2021-09-19 02:27:21 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,15 +3,15 @@
 /**
  * _isupper - ch ecks for uppercase letters
  * @c: character to check
- *
+ *int _isupper(int c)
+ *{
+ *	if (c >= 'A' && c <= 'Z')
+ *		return (1);
+ *	else
+ *		return (0);
+ *}
+
  * Return: 0 or 1
  */
 
 
-int _isupper(int c)
-{
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	else
-		return (0);
-}
